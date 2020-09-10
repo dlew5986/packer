d0c28ad24ef4ba7bcd58bc96c535f795d84a32e6
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-09-10 12:36:10 -0400

    add 2012r2 aws ami

---
+++ build_w2012r2_aws.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# exit immediately if a command returns a non-zero status
+set -e
+
+# record start
+startDateTimeStamp=$(date +%FT%T)
+start=$SECONDS
+
+# get external ip in CIDR notation
+ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+cidr=$ip/32
+echo "cidr = ${cidr}"
+
+# validate
+packer validate -var "source_cidr=${cidr}" ./w2012r2_aws.json
+
+# build
+packer build -var "source_cidr=${cidr}" -force ./w2012r2_aws.json
+
+# record end and calculate runtime duration
+end=$SECONDS
+durationTotal=$((end-start))
+
+# echo runtime
+echo "started        ${startDateTimeStamp}"
+echo "finished       $(date +%FT%T)"
+echo "total runtime  $(($durationTotal / 60))m $(($durationTotal % 60))s"
