3cb8d07a98a1ac365af0d1adcf3866a2b9b1fa05
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-09-12 13:32:24 -0400

    clean up SetupComplete.ps1

--- files/SetupComplete.ps1
+++ files/SetupComplete.ps1
@@ -1,26 +1,5 @@
 
-
-Import-Module -Name Microsoft.Powershell.Management
-New-Item -Path "C:\Temp" -ItemType Directory -Confirm:$false
-Get-ChildItem Env: | Out-File -FilePath C:\Temp\env.txt -Confirm:$false
-
-
-
-<#
-gip | Out-File -FilePath C:\Temp\gip.txt -Force -Confirm:$false
-#>
-
-<#
-Import-Module Microsoft.Powershell.LocalAccounts
-Get-LocalUser -Name "vagrantTest" | Disable-LocalUser -Confirm:$false
-#>
-
-<#
-[Environment]::SetEnvironmentVariable("TestVariable", "Test value.", "User")
-[Environment]::GetEnvironmentVariable("TestVariable","User")
-Get-Childitem Env:Computername
-#>
-
-#####################
-# future: clean this up
-#####################
+# verify SetupComplete.ps1 is working
+New-Item -Path 'C:\Temp' -ItemType Directory -Confirm:$false
+Get-ChildItem Env: | Out-File -FilePath 'C:\Temp\env.txt'
+(Get-Date).ToString() | Out-File -FilePath 'C:\Temp\date.txt'
