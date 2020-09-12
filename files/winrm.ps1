fe61d2864a366fe4d90f7c196c1b4d5abd9b037d
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-09-12 13:35:59 -0400

    change winrm connector to use ssl

--- files/winrm.ps1
+++ files/winrm.ps1
@@ -1,14 +1,53 @@
 
-# an attempt to turn off the prompt to do dumb stuff
-# if this works it should probably be moved to its own script
-#reg add HKLM\System\CurrentControlSet\Control\Network\NewNetworkWindowOff /f
-# pretty sure this^ does not work
-
 # config winrm for packer to use (must reduce to unencrypted and basic auth)
 winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 
+# disable the default firewall rules for WinRM
+Disable-NetFirewallRule -DisplayGroup 'Windows Remote Management' -Confirm:$false
 
-#####################
-# future: change this to pure powershell
-#####################
+# new firewall rule to allow WinRM 5986/tcp inbound
+$WinRmHttpsParams = @{
+    DisplayName   = "allow WinRM inbound on 5986/tcp"
+    Direction     = "Inbound"
+    Protocol      = "tcp"
+    LocalPort     = 5986
+    RemoteAddress = "Any"
+    Action        = "Allow"
+}
+New-NetFirewallRule @WinRmHttpsParams | Out-Null
+
+# remove winrm http listener
+$removeWinRmListenerParams = @{
+    ResourceURI = "winrm/config/listener"
+    SelectorSet = @{
+        Address   = "*"
+        Transport = "http"
+    }
+}
+Remove-WSManInstance @removeWinRmListenerParams
+
+# create self-signed cert
+$selfSignedCertParams = @{
+    CertStoreLocation = "Cert:\LocalMachine\My"
+    DnsName           = "packer-ami"
+    HashAlgorith      = "sha256"
+    KeyAlgorithm      = "RSA"
+    KeyLength         = 4096
+    KeyUsage          = "DigitalSignature","KeyEncipherment"
+    NotAfter          = (Get-Date).AddMonths(3)
+}
+$selfSignedCert = New-SelfSignedCertificate @selfSignedCertParams
+
+# create winrm https listener
+$newWinRmListenerParams = @{
+    ResourceURI = "winrm/config/listener"
+    SelectorSet = @{
+        Address   = "*"
+        Transport = "https"
+    }
+    ValueSet = @{
+        CertificateThumbprint = $selfSignedCert.Thumbprint
+    }
+}
+New-WSManInstance @newWinRmListenerParams
