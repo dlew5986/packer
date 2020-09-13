d24463280993418c776ea564bcd7777159da3583
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-09-12 21:41:30 -0400

    beautify quotes in winrm.ps1

--- files/winrm.ps1
+++ files/winrm.ps1
@@ -8,43 +8,43 @@ Disable-NetFirewallRule -DisplayGroup 'Windows Remote Management' -Confirm:$fals
 
 # new firewall rule to allow WinRM 5986/tcp inbound
 $WinRmHttpsParams = @{
-    DisplayName   = "allow WinRM inbound on 5986/tcp"
-    Direction     = "Inbound"
-    Protocol      = "tcp"
+    DisplayName   = 'allow WinRM inbound on 5986/tcp'
+    Direction     = 'Inbound'
+    Protocol      = 'tcp'
     LocalPort     = 5986
-    RemoteAddress = "Any"
-    Action        = "Allow"
+    RemoteAddress = 'Any'
+    Action        = 'Allow'
 }
 New-NetFirewallRule @WinRmHttpsParams | Out-Null
 
 # remove winrm http listener
 $removeWinRmListenerParams = @{
-    ResourceURI = "winrm/config/listener"
+    ResourceURI = 'winrm/config/listener'
     SelectorSet = @{
         Address   = "*"
-        Transport = "http"
+        Transport = 'http'
     }
 }
 Remove-WSManInstance @removeWinRmListenerParams
 
 # create self-signed cert
 $selfSignedCertParams = @{
-    CertStoreLocation = "Cert:\LocalMachine\My"
-    DnsName           = "packer-ami"
-    HashAlgorith      = "sha256"
-    KeyAlgorithm      = "RSA"
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    DnsName           = 'packer-ami'
+    HashAlgorith      = 'sha256'
+    KeyAlgorithm      = 'RSA'
     KeyLength         = 4096
-    KeyUsage          = "DigitalSignature","KeyEncipherment"
+    KeyUsage          = 'DigitalSignature','KeyEncipherment'
     NotAfter          = (Get-Date).AddMonths(3)
 }
 $selfSignedCert = New-SelfSignedCertificate @selfSignedCertParams
 
 # create winrm https listener
 $newWinRmListenerParams = @{
-    ResourceURI = "winrm/config/listener"
+    ResourceURI = 'winrm/config/listener'
     SelectorSet = @{
         Address   = "*"
-        Transport = "https"
+        Transport = 'https'
     }
     ValueSet = @{
         CertificateThumbprint = $selfSignedCert.Thumbprint
