def4d5a6c96e71f5492dbd6ff87e38a320f0f0e6
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-09-13 15:22:25 -0400

    simplify SetupComplete.ps1

--- files/SetupComplete.ps1
+++ files/SetupComplete.ps1
@@ -1,5 +1,3 @@
 
 # verify SetupComplete.ps1 is working
-New-Item -Path 'C:\Temp' -ItemType Directory -Confirm:$false
-Get-ChildItem Env: | Out-File -FilePath 'C:\Temp\env.txt'
-(Get-Date).ToString() | Out-File -FilePath 'C:\Temp\date.txt'
+(Get-Date).ToString() | Out-File -FilePath 'C:\Windows\Temp\SetupCompleted.date.txt'
