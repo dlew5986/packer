93129ca0c332a17230bd51d2010fd9836e645bf1
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-10-31 23:43:35 -0400

    add network tests

---
+++ packer/tests/network.Tests.ps1
@@ -0,0 +1,31 @@
+Describe "Network Validation" {
+
+    $netAdapters = Get-CimInstance -ClassName Win32_NetWorkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $true} 
+
+    Context "NetBIOS" {
+
+        foreach ($netAdapter in $netAdapters)
+        {
+            It "[$($netAdapter.Description)][Index $($netAdapter.Index)] NetBIOS over TCPIP should be disabled" {
+                $netAdapter.TcpipNetbiosOptions | should be 2
+            }
+        }
+
+    }
+
+    Context "WINS Settings" {
+
+        foreach ($netAdapter in $netAdapters)
+        {
+            It "[$($netAdapter.Description)] WINS lookups to LMHosts should be disabled" {
+                $netAdapter.WINSEnableLMHostsLookup | should be $false
+            }
+
+            It "[$($netAdapter.Description)] WINS lookups to DNS should be disabled" {
+                $netAdapter.DNSEnabledForWINSResolution | should be $false
+            }
+        }
+
+    }
+
+}
