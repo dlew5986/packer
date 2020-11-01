f6c249e014e1521dbbfbec1820d146b4b5d818ad
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-10-31 21:27:36 -0400

    add firewall and network settings to dsc config

--- packer/dsc/dsc_config.ps1
+++ packer/dsc/dsc_config.ps1
@@ -1,4 +1,5 @@
 #Requires -RunAsAdministrator
+#Requires -module NetworkingDsc
 
 Configuration DscConfig
 {
@@ -11,12 +12,6 @@ Configuration DscConfig
 
     Node $NodeName
     {
-        WindowsFeature RSAT
-        {
-            Name   = 'RSAT'
-            Ensure = 'Absent'
-        }
-
         Service PrintSpooler
         {
             Name        = 'Spooler'
@@ -24,6 +19,116 @@ Configuration DscConfig
             State       = 'Stopped'
         }
 
+        NetBios DisableNetBios
+        {
+            InterfaceAlias = '*'
+            Setting        = 'Disable'
+        }
+
+        WinsSetting ConfigureWinsSettings
+        {
+            IsSingleInstance = 'Yes'
+            EnableLMHOSTS    = $false
+            EnableDNS        = $false
+        }
+
+        FirewallProfile ConfigureDomainFirewallProfile
+        {
+            Name                  = 'Domain'
+            Enabled               = 'True'
+            DefaultInboundAction  = 'Block'
+            DefaultOutboundAction = 'Allow'
+        }
+
+        FirewallProfile ConfigurePublicFirewallProfile
+        {
+            Name                  = 'Public'
+            Enabled               = 'True'
+            DefaultInboundAction  = 'Block'
+            DefaultOutboundAction = 'Allow'
+        }
+
+        FirewallProfile ConfigurePrivateFirewallProfile
+        {
+            Name                  = 'Private'
+            Enabled               = 'True'
+            DefaultInboundAction  = 'Block'
+            DefaultOutboundAction = 'Allow'
+        }
+
+        Firewall AllowICMPv4EchoInbound
+        {
+            Name          = 'AllowICMPv4EchoInbound'
+            Ensure        = 'Present'
+            Enabled       = 'True'
+            DisplayName   = 'allow ICMPv4 echo inbound'
+            Group         = 'Custom'
+            Direction     = 'Inbound'
+            Protocol      = 'ICMPv4'
+            IcmpType      = '8'
+            RemoteAddress = 'Any'
+            Action        = 'Allow'
+        }
+
+        Firewall AllowRdpInboundOnTcp3389
+        {
+            Name          = 'AllowRdpInboundOnTcp3389'
+            Ensure        = 'Present'
+            Enabled       = 'True'
+            DisplayName   = 'allow RDP inbound on 3389/tcp'
+            Group         = 'Custom'
+            Direction     = 'Inbound'
+            Protocol      = 'tcp'
+            LocalPort     = '3389'
+            RemoteAddress = 'Any'
+            Action        = 'Allow'
+        }
+
+        Firewall AllowRdpInboundOnUdp3389
+        {
+            Name          = 'AllowRdpInboundOnUdp3389'
+            Ensure        = 'Present'
+            Enabled       = 'True'
+            DisplayName   = 'allow RDP inbound on 3389/udp'
+            Group         = 'Custom'
+            Direction     = 'Inbound'
+            Protocol      = 'udp'
+            LocalPort     = '3389'
+            RemoteAddress = 'Any'
+            Action        = 'Allow'
+        }
+
+        Firewall AllowWinRmInboundOnTcp5986
+        {
+            Name          = 'AllowWinRmInboundOnTcp5986'
+            Ensure        = 'Present'
+            Enabled       = 'True'
+            DisplayName   = 'allow WinRM inbound on 5986/tcp'
+            Group         = 'Custom'
+            Direction     = 'Inbound'
+            Protocol      = 'tcp'
+            LocalPort     = '5986'
+            RemoteAddress = 'Any'
+            Action        = 'Allow'
+        }
+
+        $displayGroupsThatShouldBeEnabled = @(
+            'Core Networking'
+            'Custom'
+        )
+
+        $rules = Get-NetFirewallRule  -Direction Inbound -Enabled True | Where-Object { $displayGroupsThatShouldBeEnabled -notcontains $_.DisplayGroup }
+
+        foreach ($rule in $rules)
+        {
+            Firewall $rule.Name
+            {
+                Name          = $rule.Name
+                Ensure        = 'Present'
+                Enabled       = 'False'
+            }
+        }
+        
     }
 }
 
@@ -37,14 +142,14 @@ DscConfig @dscConfigParams
 # create a cim session to be fed into Start-DscConfiguration
 # by default Start-DscConfiguration establishes an http-based WinRM session
 # the http WinRM listener doesn't exist (it was deleted in user_data_aws.ps1)
-# this is an ugly workaround
-# it should be corrected at some point
+# this is an ugly workaround; it should be corrected at some point
 $session = New-CimSession
 
 # invoke the lcm to process the mof file
 $startParams = @{
     CimSession   = $session
     Path         = 'C:\dsc'
+    Force        = $true
     Verbose      = $true
     Wait         = $true
 }
