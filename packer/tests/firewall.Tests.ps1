a0d3f46f7398326e8836d51bbbe21fa7f5fe7210
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-11-01 00:45:58 -0400

    add firewall profile tests

--- packer/tests/firewall.Tests.ps1
+++ packer/tests/firewall.Tests.ps1
@@ -1,9 +1,28 @@
-Describe "Firewall" {
+Describe "Firewall Validation" {
 
     $displayGroupsThatShouldBeEnabled = @(
         'Core Networking'
         'Custom'
     )
+
+    Context "Firewall profiles" {
+
+        $firewallProfiles = Get-NetFirewallProfile
+
+        foreach ($firewallProfile in $firewallProfiles)
+        {
+            It "[$($firewallProfile.Name) profile] should be enabled" {
+                $firewallProfile.Enabled | should be $true
+            }
+            It "[$($firewallProfile.Name) profile] default inbound action should be 'block'" {
+                $firewallProfile.DefaultInboundAction | should be 'block'
+            }
+            It "[$($firewallProfile.Name) profile] default outbound action should be 'allow'" {
+                $firewallProfile.DefaultOutboundAction | should be 'allow'
+            }
+        }
+
+    }
 
     Context "Inbound rules that should be enabled" {
 
@@ -18,7 +37,7 @@ Describe "Firewall" {
 
     }
 
-    Context "All other inbound rules should be disabled" {
+    Context "Inbound rules that should be disabled" {
 
         $rules = Get-NetFirewallRule -Direction Inbound | Where-Object { $displayGroupsThatShouldBeEnabled -notcontains $_.DisplayGroup } | Sort-Object -Property DisplayGroup,Profile,DisplayName
 
