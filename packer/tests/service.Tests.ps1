21ed05c2d8cdd0ea9fa2d902a9a83be91a1b13bb
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-11-01 11:01:53 -0500

    add service pester tests

---
+++ packer/tests/service.Tests.ps1
@@ -0,0 +1,11 @@
+Describe "Service Validation" {
+
+    Context "Print Spooler Validation" {
+
+        $svc = Get-Service -Name 'spooler'
+        It "should be stopped" { $svc.Status | should be 'stopped' }
+        It "startup type should be 'automatic'" { $svc.StartType | should be 'manual' }
+
+    }
+
+}
