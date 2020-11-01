f83a79f9673158b66575a365e104bb7a5caf4ba1
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-10-31 21:27:56 -0400

    add powershell prep

---
+++ packer/scripts/prep_powershell.ps1
@@ -0,0 +1,22 @@
+#Requires -RunAsAdministrator
+
+# update the help files because reasons
+Update-Help -Force -ErrorAction SilentlyContinue
+
+# install the NuGet package provider to enable pulling modules down from the PowerShell Gallery
+$nugetParams = @{
+    Name    = 'NuGet'
+    Scope   = 'AllUsers'
+    Force   = $true
+    Verbose = $true
+}
+Install-PackageProvider @nugetParams
+
+# install various modules from the PowerShell Gallery
+$modulesParams = @{
+    Name    = 'PSDscResources','NetworkingDsc'
+    Scope   = 'AllUsers'
+    Force   = $true
+    Verbose = $true
+}
+Install-Module @modulesParams
