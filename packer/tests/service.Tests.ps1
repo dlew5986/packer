fc0a6e2adfbb97ff70aa3d3eaf2deda9531f0c49
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2020-11-01 13:23:16 -0500

    refactor spacing in service tests

--- packer/tests/service.Tests.ps1
+++ packer/tests/service.Tests.ps1
@@ -1,10 +1,15 @@
 Describe "Service Validation" {
 
-    Context "Print Spooler Validation" {
+    Context "Print Spooler" {
 
         $svc = Get-Service -Name 'spooler'
-        It "should be stopped" { $svc.Status | should be 'stopped' }
-        It "startup type should be 'automatic'" { $svc.StartType | should be 'manual' }
+
+        It "should be stopped" {
+            $svc.Status | should be 'stopped'
+        }
+        It "startup type should be 'automatic'" {
+            $svc.StartType | should be 'manual'
+        }
 
     }
 
