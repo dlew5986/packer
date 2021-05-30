9e1948345e48f2181aa8c963dc01165e390085de
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-05-29 20:46:03 -0400

    add iam objects for packer

---
+++ terraform/iam/role_packer.tf
@@ -0,0 +1,37 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [var.principal_arn_umbrella]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "red_packer" {
+  provider           = aws.red
+  name               = "redPacker"
+  description        = "packer description"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "red_packer" {
+  provider   = aws.red
+  role       = aws_iam_role.red_packer.name
+  policy_arn = aws_iam_policy.policy_red_packer.arn
+}
+
+resource "aws_iam_role" "blue_packer" {
+  provider           = aws.blue
+  name               = "bluePacker"
+  description        = "packer description"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "blue_packer" {
+  provider   = aws.blue
+  role       = aws_iam_role.blue_packer.name
+  policy_arn = aws_iam_policy.policy_blue_packer.arn
+}
---
+++ terraform/iam/variables.tf
@@ -0,0 +1,11 @@
+variable "assume_role_arn_red" {
+  type = string
+}
+
+variable "assume_role_arn_blue" {
+  type = string
+}
+
+variable "principal_arn_umbrella" {
+  type = string
+}
